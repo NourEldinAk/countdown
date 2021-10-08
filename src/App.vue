673895a8de7a5a8214d17cc0d5079541c3df1a81
NourEldinAk <noekatsu44@gmail.com>
2021-10-08 15:39:09 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,149 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header>
+    <div>We're Launching Soon</div>
+  </header>
+  <div class="parent-container">
+    <the-card title="days" :number="days"></the-card>
+    <the-card title="hours" :number="hours"></the-card>
+
+    <the-card title="minutes" :number="minutes"></the-card>
+    <the-card title="seconds" :number="seconds"></the-card>
+  </div>
+  <footer>
+    <div class="contact">
+      <img src="./images/icon-facebook.svg" alt="" />
+      <img src="./images/icon-pinterest.svg" alt="" />
+      <img src="./images/icon-instagram.svg" alt="" />
+    </div>
+    <img src="./images/pattern-hills.svg" alt="pattern-hills" />
+  </footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  import TheCard from "./components/TheCard.vue";
+  export default {
+    data() {
+      return {
+        days: 14,
+        hours: 23,
+        minutes: 59,
+        seconds: 59,
+        timer: null,
+        end: false,
+      };
+    },
+    components: {
+      TheCard,
+    },
+    watch: {
+      days(value) {
+        if (value < 0) {
+          this.endTimer();
+        }
+      },
+      hours(value) {
+        if (value === 0 && !this.end) {
+          this.hours = 23;
+          this.getDays();
+        }
+      },
+      minutes(value) {
+        if (value === 0 && !this.end) {
+          this.minutes = 59;
+          this.getHours();
+        }
+      },
+      seconds(value) {
+        if (value === 0 && !this.end) {
+          this.getMinutes();
+          this.seconds = 59;
+        }
+      },
+    },
+    methods: {
+      getMinutes() {
+        --this.minutes;
+      },
+      getHours() {
+        --this.hours;
+      },
+      getDays() {
+        --this.days;
+      },
+      endTimer() {
+        this.end = true;
+        clearInterval(this.timer);
+        this.days = 0;
+        this.hours = 0;
+        this.minutes = 0;
+        this.seconds = 0;
+      },
+    },
+    created() {
+      this.timer = setInterval(() => {
+        --this.seconds;
+      }, 1000);
+    },
+  };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Red+Hat+Text:wght@700&display=swap");
+  * {
+    box-sizing: border-box;
+  }
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 14px;
+    height: 100%;
+    font-family: "Red Hat Text";
+    background-color: hsl(235, 16%, 14%);
+    background-image: url("./images/bg-stars.svg");
+    background-size: cover;
+  }
+  #app {
+    height: 100%;
+  }
+  header {
+    height: 30%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+    font-size: 1.4rem;
+    letter-spacing: 8px;
+    text-transform: uppercase;
+  }
+  .parent-container {
+    height: calc(50% - 140px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    .contact {
+      position: absolute;
+      width: 15%;
+      top: 50%;
+      display: flex;
+      justify-content: space-around;
+      img {
+        cursor: pointer;
+        width: 25px;
+        height: auto;
+        &:hover {
+          filter: contrast(20);
+        }
+      }
+    }
+    img {
+      width: 100%;
+    }
+  }
 </style>
---
+++ src/components/TheCard.vue
@@ -0,0 +1,155 @@
+<template>
+  <div class="parent">
+    <div class="container">
+      <div class="card1" ref="card1">
+        <div class="number">{{ 10 > number ? "0" + number : number }}</div>
+      </div>
+      <div class="card2">
+        <div class="number">{{ 10 > number ? "0" + number : number }}</div>
+      </div>
+    </div>
+    <p>{{ title }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    props: ["title", "number"],
+    data() {
+      return {
+        flip: false,
+      };
+    },
+
+    created() {},
+  };
+</script>
+
+<style lang="scss" scoped>
+  .parent {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    margin-left: 50px;
+    p {
+      color: hsl(237, 18%, 59%);
+      font-size: 1.1rem;
+      letter-spacing: 8px;
+      margin-top: 15px;
+      text-transform: uppercase;
+    }
+  }
+  .container {
+    overflow: hidden;
+    height: 170px;
+    width: 170px;
+    background-color: hsl(236, 21%, 26%);
+    border-radius: 10px;
+    perspective: 400px;
+
+    .card1,
+    .card2 {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 7rem;
+      height: 47%;
+      color: hsl(345, 95%, 68%);
+      background-color: hsl(236, 21%, 26%);
+    }
+    // .flipCard {
+    //   animation: cardFlip 1s ease-out;
+    // }
+
+    .card1 {
+      overflow: hidden;
+
+      z-index: 2;
+      filter: brightness(0.93);
+
+      border-bottom: 1px solid hsl(234, 17%, 12%);
+      border-radius: 8px 8px 0 0;
+      transform-origin: bottom;
+      transform-style: preserve-3d;
+
+      .number {
+        position: absolute;
+        bottom: -15px;
+        height: 100%;
+      }
+      &::before {
+        content: "";
+        position: absolute;
+        height: 17px;
+        width: 17px;
+        z-index: 2;
+
+        bottom: -10px;
+        border-radius: 50%;
+        left: -10px;
+        background-color: hsl(234, 17%, 12%);
+      }
+      &::after {
+        content: "";
+        position: absolute;
+        height: 17px;
+        width: 17px;
+        bottom: -10px;
+        z-index: 2;
+
+        border-radius: 50%;
+        right: -10px;
+        background-color: hsl(234, 17%, 12%);
+      }
+    }
+    .card2 {
+      z-index: 1;
+      background-color: #3a3c58;
+      box-shadow: 0px 12px 1px hsl(234, 17%, 12%);
+
+      border-radius: 0 0 8px 8px;
+
+      .number {
+        position: absolute;
+        top: -66px;
+        height: 100%;
+      }
+      &::before {
+        content: "";
+        position: absolute;
+        height: 17px;
+        width: 17px;
+        top: -10px;
+        border-radius: 50%;
+        z-index: 2;
+
+        left: -10px;
+        background-color: hsl(234, 17%, 12%);
+      }
+      &::after {
+        content: "";
+        position: absolute;
+        height: 17px;
+        z-index: 2;
+        width: 17px;
+        top: -10px;
+        border-radius: 50%;
+        right: -10px;
+        background-color: hsl(234, 17%, 12%);
+      }
+    }
+  }
+  // @keyframes cardFlip {
+  //   0% {
+  //     transform: rotateX(0deg);
+  //   }
+
+  //   90% {
+  //     opacity: 1;
+  //   }
+  //   100% {
+  //     transform: rotateX(-93deg);
+  //     opacity: 0;
+  //   }
+  // }
+</style>
